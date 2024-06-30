cd7a0c639d4382e50e8075e97d6e7d1a52517c3b
kimHyeonJu <kimhyesom1202@gmail.com>
2024-07-01 03:15:14 +0900

    Add cards data

---
+++ js-homework/mission-3/client/data/data.js
@@ -0,0 +1,264 @@
+const data = [
+  {
+    id: 0,
+    name: 'the_arrow',
+    img_url: '../../assets/images/the_arrow.png'
+  },
+  {
+    id: 1,
+    name: 'the_big',
+    img_url: '../../assets/images/the_big.png'
+  },
+  {
+    id: 2,
+    name: 'the_bubbles',
+    img_url: '../../assets/images/the_bubbles.png'
+  },
+  {
+    id: 2,
+    name: 'the_change',
+    img_url: '../../assets/images/the_change.png'
+  },
+  {
+    id: 3,
+    name: 'the_cloud',
+    img_url: '../../assets/images/the_cloud.png'
+  },
+  {
+    id: 4,
+    name: 'the_create',
+    img_url: '../../assets/images/the_create.png'
+  },
+  {
+    id: 5,
+    name: 'the_dark',
+    img_url: '../../assets/images/the_dark.png'
+  },
+  {
+    id: 6,
+    name: 'the_dash',
+    img_url: '../../assets/images/the_dash.png'
+  },
+  {
+    id: 7,
+    name: 'the_dream',
+    img_url: '../../assets/images/the_dream.png'
+  },
+  {
+    id: 8,
+    name: 'the_earth',
+    img_url: '../../assets/images/the_earth.png'
+  },
+  {
+    id: 9,
+    name: 'the_erase',
+    img_url: '../../assets/images/the_erase.png'
+  },
+  {
+    id: 10,
+    name: 'the_fight',
+    img_url: '../../assets/images/the_fight.png'
+  },
+  {
+    id: 11,
+    name: 'the_fire',
+    img_url: '../../assets/images/the_fire.png'
+  },
+  {
+    id: 12,
+    name: 'the_float',
+    img_url: '../../assets/images/the_float.png'
+  },
+  {
+    id: 13,
+    name: 'the_flower',
+    img_url: '../../assets/images/the_flower.png'
+  },
+  {
+    id: 14,
+    name: 'the_fly',
+    img_url: '../../assets/images/the_fly.png'
+  },
+  {
+    id: 15,
+    name: 'the_freeze',
+    img_url: '../../assets/images/the_freeze.png'
+  },
+  {
+    id: 16,
+    name: 'the_glow',
+    img_url: '../../assets/images/the_glow.png'
+  },
+  {
+    id: 17,
+    name: 'the_illusion',
+    img_url: '../../assets/images/the_illusion.png'
+  },
+  {
+    id: 18,
+    name: 'the_jump',
+    img_url: '../../assets/images/the_jump.png'
+  },
+  {
+    id: 19,
+    name: 'the_libra',
+    img_url: '../../assets/images/the_libra.png'
+  },
+  {
+    id: 20,
+    name: 'the_light',
+    img_url: '../../assets/images/the_light.png'
+  },
+  {
+    id: 21,
+    name: 'the_little',
+    img_url: '../../assets/images/the_little.png'
+  },
+  {
+    id: 22,
+    name: 'the_lock',
+    img_url: '../../assets/images/the_lock.png'
+  },
+  {
+    id: 23,
+    name: 'the_loop',
+    img_url: '../../assets/images/the_loop.png'
+  },
+  {
+    id: 24,
+    name: 'the_maze',
+    img_url: '../../assets/images/the_maze.png'
+  },
+  {
+    id: 25,
+    name: 'the_mirror',
+    img_url: '../../assets/images/the_mirror.png'
+  },
+  {
+    id: 26,
+    name: 'the_mist',
+    img_url: '../../assets/images/the_mist.png'
+  },
+  {
+    id: 27,
+    name: 'the_move',
+    img_url: '../../assets/images/the_move.png'
+  },
+  {
+    id: 28,
+    name: 'the_power',
+    img_url: '../../assets/images/the_power.png'
+  },
+  {
+    id: 29,
+    name: 'the_rain',
+    img_url: '../../assets/images/the_rain.png'
+  },
+  {
+    id: 30,
+    name: 'the_return',
+    img_url: '../../assets/images/the_return.png'
+  },
+  {
+    id: 31,
+    name: 'the_sand',
+    img_url: '../../assets/images/the_sand.png'
+  },
+  {
+    id: 32,
+    name: 'the_shadow',
+    img_url: '../../assets/images/the_shadow.png'
+  },
+  {
+    id: 33,
+    name: 'the_shield',
+    img_url: '../../assets/images/the_shield.png'
+  },
+  {
+    id: 34,
+    name: 'the_shot',
+    img_url: '../../assets/images/the_shot.png'
+  },
+  {
+    id: 35,
+    name: 'the_silent',
+    img_url: '../../assets/images/the_silent.png'
+  },
+  {
+    id: 36,
+    name: 'the_sleep',
+    img_url: '../../assets/images/the_sleep.png'
+  },
+  {
+    id: 37,
+    name: 'the_snow',
+    img_url: '../../assets/images/the_snow.png'
+  },
+  {
+    id: 38,
+    name: 'the_song',
+    img_url: '../../assets/images/the_song.png'
+  },
+  {
+    id: 39,
+    name: 'the_storm',
+    img_url: '../../assets/images/the_storm.png'
+  },
+  {
+    id: 40,
+    name: 'the_sweet',
+    img_url: '../../assets/images/the_sweet.png'
+  },
+  {
+    id: 41,
+    name: 'the_sword',
+    img_url: '../../assets/images/the_sword.png'
+  },
+  {
+    id: 42,
+    name: 'the_through',
+    img_url: '../../assets/images/the_through.png'
+  },
+  {
+    id: 43,
+    name: 'the_thunder',
+    img_url: '../../assets/images/the_thunder.png'
+  },
+  {
+    id: 44,
+    name: 'the_time',
+    img_url: '../../assets/images/the_time.png'
+  },
+  {
+    id: 45,
+    name: 'the_twin',
+    img_url: '../../assets/images/the_twin.png'
+  },
+  {
+    id: 46,
+    name: 'the_voice',
+    img_url: '../../assets/images/the_voice.png'
+  },
+  {
+    id: 47,
+    name: 'the_watery',
+    img_url: '../../assets/images/the_watery.png'
+  },
+  {
+    id: 48,
+    name: 'the_wave',
+    img_url: '../../assets/images/the_wave.png'
+  },
+  {
+    id: 49,
+    name: 'the_windy',
+    img_url: '../../assets/images/the_windy.png'
+  },
+  {
+    id: 50,
+    name: 'the_wood',
+    img_url: '../../assets/images/the_wood.png'
+  }
+]
+
+export default data;
